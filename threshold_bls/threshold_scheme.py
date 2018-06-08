39c689c5d4258ee9b6fcb261f4c8d9ee80891f00
Carl Beekhuizen <carl.beek@gmail.com>
2018-06-08 10:20:15 +0200

    Working Threshold Signatures! (But requires centralised Dealer)

--- threshold_bls/threshold_scheme.py
+++ threshold_bls/threshold_scheme.py
@@ -9,61 +9,58 @@ n = None
 
 
 def key_gen():
+    # Todo: Move away from centralised key_generation
     if t is None or n is None:
         raise Exception('t or n not initialized')
     if t > n or t <= 0:
         raise AssertionError('t must be <= n and t > 0')
-    # Todo: Finnish me!!
-    return None
-
-
-def key_gen_1():
-    rng = SystemRandom()
-    a = [rng.randint(0, int(G.ord)) for i in range(2 * t + 1)]
-    b = [rng.randint(0, int(G.ord)) for i in range(2 * t + 1)]
-    sigmas = list(map(lambda j: poly_eval(a, j), range(0, t))) % q
-    rhos = list(map(lambda j: poly_eval(b, j), range(0, t))) % q
-    As = [e(g*ak, h*bk) for (ak, bk) in (a, b)]
-    return sigmas, rhos, As
-
-
-def key_gen_2(sigmas, rhos, As):
-    sigma = sum(sigmas) % q
-    rho = sum(rhos) % q
-    A = reduce(lambda a, b: a * b, As) % p
-    return sigma, rho, A
-
-def key_gen_3(a):
-    return None
+    v = [o.random() for _ in range(0, t)]
+    sk = [poly_eval(v, i) % o for i in range(1, n + 1)]
+    pk = [xi*g2 for xi in sk]
+    return sk, pk
 
 
 def partial_sign(m, sk):
     return bls.sign(m, sk)
 
 
-def partial_verify(m, pk):
-    return verify(m, pk)
+def partial_verify(pk, sig, m):
+    return verify(pk, sig, m)
 
 
 def public_key_reconstruction(pks):
-    # Todo: Finnish me!!
-    return None
+    pks = [pks[i] * lagrange_coefficient(t, o, i + 1) for i in range(t)]
+    pk = reduce(lambda a, b: a + b, pks)
+    return pk
 
 
 def signature_reconstruction(m, sigmas):
-    # Filter out the invalid signatures by checking all of them (required for robustness)
-    sigmas = filter(lambda a: partial_verify(m, a), sigmas)
+    # # Filter out the invalid signatures by checking all of them (required for robustness)
+    # sigmas = list(filter(lambda a: partial_verify(m, a), sigmas))
 
     # Check that there are sufficient signatures remaining
     if len(sigmas) <= t:
         raise AssertionError('At least t + 1 valid signatures are needed to recover a valid group signature.')
 
     # Raise each sigma to it's lagrangian co-efficient
-    sigmas = [sigmas[i]**lagrange_coefficient(t, o, i+1) for i in range(t)]
+    sigmas = [sigmas[i]*lagrange_coefficient(t, o, i+1) for i in range(t)]
     # combine sigmas via multiplication
-    sigma = reduce(lambda a, b: a*b, sigmas)
+    sigma = reduce(lambda a, b: a+b, sigmas)
     return sigma
 
 
-def verify(m, pk):
-    return bls.verify(m, pk)
+def verify(pk, sig, m):
+    return bls.verify(pk, sig, m)
+
+
+if __name__ == '__main__':
+    t = 3
+    n = 6
+
+    (sks, pks) = key_gen()
+    pk = public_key_reconstruction(pks)
+    m = 'Banana'
+    sigmas = [partial_sign(m, s) for s in sks]
+    ver = [partial_verify(pks[i], sigmas[i], m) for i in range(len(sigmas))]
+    sigma = signature_reconstruction(m, sigmas)
+    print(verify(pk, sigma, m))
