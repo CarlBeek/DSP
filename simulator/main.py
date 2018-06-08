9ec18411e82c24eca7318c366c7d347d4cd5c488
Carl Beekhuizen <carl.beek@gmail.com>
2018-06-08 17:08:41 +0200

    Inboxes as dict of Queue()

--- simulator/main.py
+++ simulator/main.py
@@ -1,19 +1,35 @@
-import multiprocessing
+from multiprocessing import Process, Queue
+from random import random
 NUM_NODES = 100
 
 
-def node(inbox, id):
-    for k, v in inbox.items():
-        v.put('Hi %d, I am %d' % (k, id))
+class Node:
+    def __init__(self, inbox, uid):
+        self.inbox = inbox
+        self.id = uid
+        self.num_nodes = len(inbox)
+
+    def _gossip(self, m, p=.5):
+        for k, v in self.inbox.items():
+            if random() < p and k != self.id:
+                v.put(m)
+
+    def _broadcast(self, m):
+        for k, v in self.inbox.items():
+            if k != self.id:
+                v.put(m)
+
+    def _send(self, m, dest):
+        try:
+            self.inbox[dest].put(m)
+        except KeyError:
+            print('Node doesnt exist')
 
 
 if __name__ == '__main__':
-    mgr = multiprocessing.Manager()
-    communal_inbox = dict((i, multiprocessing.Queue()) for i in range(NUM_NODES))
-    jobs = [multiprocessing.Process(target=node, args=(communal_inbox, i)) for i in range(10)]
+    communal_inbox = dict((i, Queue()) for i in range(NUM_NODES))
+    jobs = [Process(target=Node, args=(communal_inbox, i)) for i in range(NUM_NODES)]
     for j in jobs:
         j.start()
     for j in jobs:
         j.join()
-    for _ in range(10):
-        print(communal_inbox[5].get())
