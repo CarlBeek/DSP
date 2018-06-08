2dbc4a12bfc16d4a35339e2cf054c4aa2b4d783b
Carl Beekhuizen <carl.beek@gmail.com>
2018-06-08 16:32:50 +0200

    Python Multiprocessing is just so much sexier

--- simulator/main.py
+++ simulator/main.py
@@ -1,42 +1,19 @@
-from mpi4py import MPI
-import random
-from time import sleep
+import multiprocessing
+NUM_NODES = 100
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
 
-# connected_nodes_out = [random.choice(range(size)) for _ in range(6)]
-connected_nodes_out = range(size)
+def node(inbox, id):
+    for k, v in inbox.items():
+        v.put('Hi %d, I am %d' % (k, id))
 
-for i in connected_nodes_out:
-    if i != rank:
-        comm.isend('Hi ' + str(i) + ' I am ' + str(rank), i)
 
-#
-# def is_new_messages(req):
-#     # print(req.test())
-#     return req.test()[1] is not None
-#
-#
-# def get_new_msg(req):
-#     msg = req.wait()
-#     return msg
-#
-#
-# def process_new_message(req):
-#     print(get_new_msg(req))
-#
-#
-# sleep(0.5)
-# r = comm.irecv()
-# # for _ in range(1000):
-# while True:
-#     if is_new_messages(r):
-#         process_new_message(r)
-#     else:
-#         sleep(0.005)
-
-while True:
-    print(comm.recv())
-    sleep(0.1)
+if __name__ == '__main__':
+    mgr = multiprocessing.Manager()
+    communal_inbox = dict((i, multiprocessing.Queue()) for i in range(NUM_NODES))
+    jobs = [multiprocessing.Process(target=node, args=(communal_inbox, i)) for i in range(10)]
+    for j in jobs:
+        j.start()
+    for j in jobs:
+        j.join()
+    for _ in range(10):
+        print(communal_inbox[5].get())
